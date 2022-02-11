e3add6cee80834feb0b3d1426b976fecd3f8cc3b
nguyendev2018 <nguyendev2018@gmail.con>
2022-02-11 14:45:46 +0700

    a

--- js/header.js
+++ js/header.js
@@ -1,22 +1,22 @@
-// document.addEventListener("DOMContentLoaded", function() {
-//     $(".header-mobile li").on("click", function(e) {
-//         e.stopPropagation();
-//         var element = $(this);
-//         if (element.hasClass("open")) {
-//             element.removeClass("open");
-//             element.find("li").removeClass("open");
-//             element.find("ul").slideUp(400,
-//                 "swing");
-//         } else {
-//             element.addClass("open");
-//             element.children("ul").slideDown(400, "swing");
-//             element.siblings("li").children("ul").slideUp(400, "swing");
-//             element.siblings("li").removeClass("open");
-//             element.siblings("li").find("li").removeClass("open");
-//             element.siblings("li").find("ul").slideUp(400, "swing");
-//         }
-//     })
-// });
+document.addEventListener("DOMContentLoaded", function() {
+    $(".header-mobile li").on("click", function(e) {
+        e.stopPropagation();
+        var element = $(this);
+        if (element.hasClass("open")) {
+            element.removeClass("open");
+            element.find("li").removeClass("open");
+            element.find("ul").slideUp(400,
+                "swing");
+        } else {
+            element.addClass("open");
+            element.children("ul").slideDown(400, "swing");
+            element.siblings("li").children("ul").slideUp(400, "swing");
+            element.siblings("li").removeClass("open");
+            element.siblings("li").find("li").removeClass("open");
+            element.siblings("li").find("ul").slideUp(400, "swing");
+        }
+    })
+});
 // let box_button = $(".box-button");
 // for (let i = 0; i < box_button.length; i++) {
 //     $(box_button[i]).on("click", function() {
