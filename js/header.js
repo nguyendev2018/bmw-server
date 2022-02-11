c97257bf9b8efffd750b2ec37aa81141b6e1a4fe
nguyendev2018 <nguyendev2018@gmail.con>
2022-02-11 13:25:36 +0700

    u

--- js/header.js
+++ js/header.js
@@ -8,33 +8,10 @@ for (let i = 0; i < box_button.length; i++) {
 //ANCHOR js menu
 
 window.addEventListener("scroll", function() {
-        let header = document.querySelector(".header-menu");
-        header.classList.toggle("fix-top", window.scrollY > 1000)
-    })
-    //ANCHOR login-register
-let nut = document.querySelectorAll('.item-click');
-let btn_cancel = document.querySelectorAll('.btn-cancel');
-let layer_black = document.querySelector('.layer-black');
-let icon_bars = document.querySelectorAll('.icon-bars');
-let menu_mobile = document.querySelector('.header-menu--list_item');
-for (let i = 0; i < nut.length; i++) {
-    nut[i].addEventListener("click", function() {
-        var content_show = this.getAttribute("data-show");
-        var current_show = document.getElementById(content_show);
-        current_show.classList.toggle("active");
-        layer_black.classList.add("active");
-        current_show.querySelector(".btn-cancel").onclick = function() {
-            current_show.classList.remove("active");
-            layer_black.classList.remove("active");
-        }
-    });
-}
-// icon-bars add/close
-icon_bars.forEach(element => {
-    element.addEventListener("click", () => {
-        menu_mobile.classList.toggle("active");
-    })
-});
+    let header = document.querySelector(".header-menu");
+    header.classList.toggle("fix-top", window.scrollY > 1000)
+})
+
 //ANCHOR load header 
 $(".header-mobile li").on("click", function(e) {
     e.stopPropagation();
