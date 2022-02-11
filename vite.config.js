8082d3604863cf5932eaa5723a48dc2d3dadc75e
nguyendev2018 <nguyendev2018@gmail.con>
2022-02-11 12:12:52 +0700

    --- Auto-Git Commit ---

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,8 @@ module.exports = defineConfig({
             input: {
                 main: resolve(__dirname, "index.html"),
                 blogDetails: resolve(__dirname, "blog-details.html"),
-                register: resolve(__dirname, "register.html")
+                register: resolve(__dirname, "register.html"),
+                login: resolve(__dirname, "login.html")
             }
         }
     }
